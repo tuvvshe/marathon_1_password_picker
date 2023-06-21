067434395670520ac25cd96d25ea89b86cb9c53c
22 <87944616+tuvvshe@users.noreply.github.com>
2023-06-21 23:27:01 +0200

    Add files via upload

---
+++ password_picker.py
@@ -0,0 +1,11 @@
+import  random
+import string
+print("WELCOME TO THE PASSWORD PICKER")
+adjectives = ['nice', 'red', 'green'
+              'grown', 'teneg', 'mangar'
+              'baina', 'baihgui']
+nouns = ['pasta', 'pizza', 'cheese', 'soup', 'baguette',
+         'bansh', 'dumplings', 'chicken']
+
+adjective = random.choice(adjectives)
+noun = random.choice(nouns)
